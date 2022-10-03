bb4772db03634e851b3926f832ac5823b0b5d750
Albert <albert.fedorov.99@bk.ru>
2022-10-03 22:32:31 +0300

    нахождение максимального числа их трех введенных

---
+++ Task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+int count = 0;
+while (count < 4)
+{
+    if (num1 > max)
+    {
+        max = num1;
+        Console.WriteLine("Максимальное число " + num1);
+    }
+    if (num2 > max)
+    {
+        max = num2;
+        Console.WriteLine("Максимальное число " + num2);
+    }
+    if (num3 > max)
+    {
+        max = num3;
+        Console.WriteLine("Максимальное число " + num3);
+    }
+    count++;
+}
