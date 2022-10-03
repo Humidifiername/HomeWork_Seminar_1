cd70a92883a5ba980a105f9c6636cca366cf1d01
Albert <albert.fedorov.99@bk.ru>
2022-10-03 22:43:53 +0300

    нахождение четного числа

---
+++ Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает является ли число четным
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
